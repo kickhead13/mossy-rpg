ac970bb36943200a6b3a6c4bbefbfd71f592fca4
kickhead13 <alex_cioltea93@yahoo.com>
2023-11-30 16:19:03 +0200

    more quality of life changes

--- src/main.rs
+++ src/main.rs
@@ -13,37 +13,30 @@ impl Position {
 }
 
 fn main() {
-    
-    let (sender, receiver) = mpsc::channel::<u8>();
+    let character_sprite: &str = ":3";
 
+    let (sender, receiver) = mpsc::channel::<u8>();
     let getter = Getch::new();
 
     let _input_thread = std::thread::spawn( move || {
-    
         loop {
             if let Ok(input) = getter.getch() {
                 sender.send(input as u8);
             }
         }
-
     });
     
-
     let mut char_pos: Position = Position::new(1,1);
     let (mut term_width, mut term_height) = terminal::size().unwrap();
     let mut stdout = stdout();
     stdout.queue(terminal::Clear(terminal::ClearType::All)).unwrap();
-
     let mut fps_contor: u32 = 0;
 
     loop {
-        
         stdout.queue(terminal::Clear(terminal::ClearType::All)).unwrap();
-
         fps_contor += 1;
         stdout.queue(cursor::MoveTo(2,0)).unwrap();
         stdout.write(format!("{}",fps_contor).as_bytes()).unwrap();
-
         if let Ok(input) = receiver.try_recv() {
            match input {
                 100 | 68 => char_pos.0 += 1, // D or d is pressed
@@ -64,20 +57,14 @@ fn main() {
             }
         } 
         else {
-        
             stdout.queue(cursor::MoveTo(char_pos.0, char_pos.1)).unwrap();
-            stdout.write(":3".as_bytes()).unwrap();
-
+            stdout.write(character_sprite.as_bytes()).unwrap();
             (term_width, term_height) = terminal::size().unwrap();
-
             stdout.queue(cursor::MoveTo(0,0)).unwrap();
             stdout.write("a".as_bytes()).unwrap();
-    
-
             stdout.queue(cursor::MoveTo(term_width, term_height)).unwrap();
             stdout.write("b".as_bytes()).unwrap();
             stdout.flush();
-
             std::thread::sleep(Duration::from_millis(100));
         }
     }
