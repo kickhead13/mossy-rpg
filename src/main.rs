f8f185cae66c0855db0f323a6927eb00321309f2
kickhead13 <alex_cioltea93@yahoo.com>
2023-11-30 17:09:53 +0200

    movement is no longer broken

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,21 @@ impl Position {
 }
 
 fn main() {
+
+    let mut is_next_frame = true;
+
     let character_sprite: &str = ":3";
+    let sleep_duration = Duration::from_millis(100);
 
     let (sender, receiver) = mpsc::channel::<u8>();
-    let getter = Getch::new();
+    let mut getter = Getch::new();
 
     let _input_thread = std::thread::spawn( move || {
         loop {
             if let Ok(input) = getter.getch() {
                 sender.send(input as u8);
+                //std::thread::sleep(sleep_duration);
+                //getter = Getch::new();
             }
         }
     });
@@ -38,24 +44,27 @@ fn main() {
         stdout.queue(cursor::MoveTo(2,0)).unwrap();
         stdout.write(format!("{}",fps_contor).as_bytes()).unwrap();
         if let Ok(input) = receiver.try_recv() {
-           match input {
-                100 | 68 => char_pos.0 += 1, // D or d is pressed
-                83 | 115 => char_pos.1 +=1, // S or s is pressed
-                87 | 119 => {
+            if is_next_frame {
+                match input {
+                    100 | 68 => char_pos.0 += 1, // D or d is pressed
+                    83 | 115 => char_pos.1 +=1, // S or s is pressed
+                    87 | 119 => {
                         // W or w is pressed
                         if char_pos.1 > 1 {
                             char_pos.1 -= 1;
                         }
                     },
-                97 | 65 => {
+                    97 | 65 => {
                         // A or a is pressed
                         if char_pos.0 > 0 {
                             char_pos.0 -= 1;
                         }
                     },
-                Error => ()
+                    Error => ()
+                }
+                is_next_frame = false;
             }
-        } 
+        }
         else {
             stdout.queue(cursor::MoveTo(char_pos.0, char_pos.1)).unwrap();
             stdout.write(character_sprite.as_bytes()).unwrap();
@@ -65,7 +74,8 @@ fn main() {
             stdout.queue(cursor::MoveTo(term_width, term_height)).unwrap();
             stdout.write("b".as_bytes()).unwrap();
             stdout.flush();
-            std::thread::sleep(Duration::from_millis(100));
+            std::thread::sleep(sleep_duration);
+            is_next_frame = true;
         }
     }
 }
